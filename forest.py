3d573f6bb9379808c05664ae542c5d934d913210
Brandon Helfrich <bran.helfrich@gmail.com>
2022-05-30 12:20:53 -0500

    adds intro//run forest.py to play

--- forest.py
+++ forest.py
@@ -1,6 +1,7 @@
 # Continuing my decision-based adventure typing game to practice my Python skills.
 # Author: Brandon Helfrich
 
+import intro   # Adds the introdcution. If you want to play, run this file.
 import sys
 import time
 
@@ -27,3 +28,13 @@ def typingInput(text):
 time.sleep(3)
 
 typingPrint("The Dark Forest, as it's known those inside of the walls, is the home of many dark creatures.\n")
+typingPrint("It borders the Great City walls to keep enemies out, but also those who live within from curiosity.\n\n")
+
+time.sleep(1)
+
+typingPrint("You instantly feel the eeriness as you enter the shadows, you feel as if there are a thousand eyes\n")
+typingPrint("on you, watching, waiting...\n\n")
+
+time.sleep(1)
+
+# CONTIUNE HERE
